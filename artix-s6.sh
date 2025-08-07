ac9e9d81fe7d6f686a118913857eebe29f1fcd45
Onishell Tiburcio <46875497+OnishellT@users.noreply.github.com>
2025-08-07 05:19:35 -0400

    Update artix-s6.sh

--- artix-s6.sh
+++ artix-s6.sh
@@ -1,132 +1,157 @@
-#!/bin/bash
+#!/bin/sh
 
 # Check if the script is run as root
-if [ "$EUID" -ne 0 ]; then
-    echo "Please run as root"
+if [ "$(id -u)" -ne 0 ]; then
+    echo "Please run this script as root."
     exit 1
 fi
 
-# Get the username from the first argument
-USER=$1
+# Set target disk and partitions (adjust as needed)
+TARGET_DISK="/dev/sda"
+BOOT_PART="${TARGET_DISK}1"
+ROOT_PART="${TARGET_DISK}2"
 
-# Check if the user exists
-if ! id "$USER" &>/dev/null; then
-    echo "User $USER does not exist"
-    exit 1
-fi
+# Partition the disk
+echo "Partitioning the disk..."
+parted -s "$TARGET_DISK" mklabel gpt
+parted -s "$TARGET_DISK" mkpart primary 0% 512MiB
+parted -s "$TARGET_DISK" set 1 boot on
+part Target_DISK="${TARGET_DISK}2"
 
-# Install necessary packages (including Pipewire and Bluetooth)
-pacman -S --needed hyprland waybar kitty sddm swaybg ttf-dejavu xwayland \
-    pipewire pipewire-pulse pipewire-alsa pipewire-jack wireplumber \
-    bluez bluez-utils blueman
+# Partition the disk
+echo "Partitioning the disk..."
+parted -s "$TARGET_DISK" mklabel gpt
+parted -s "$TARGET_DISK" mkpart primary 0% 512MiB
+parted -s "$TARGET_DISK" set 1 boot on
+parted -s "$TARGET_DISK" mkpart primary 512MiB 100%
 
-# Enable sddm and bluetooth services
-systemctl enable sddm
-systemctl enable bluetooth
+# Format partitions
+echo "Formatting partitions..."
+mkfs.fat -F32 "$BOOT_PART"
+mkfs.ext4 "$ROOT_PART"
 
-# Create config directories
-mkdir -p /home/$USER/.config/hypr
-mkdir -p /home/$USER/.config/waybar
+# Mount partitions
+echo "Mounting partitions..."
+mount "$ROOT_PART" /mnt
+mkdir /mnt/boot
+mount "$BOOT_PART" /mnt/boot
 
-# Write Hyprland configuration
-cat << EOF > /home/$USER/.config/hypr/hyprland.conf
-# Hyprland configuration
-monitor=,preferred,auto,1
+# Install base system and essential packages
+echo "Installing base system and packages..."
+basestrap /mnt base base-devel dinit dinit-artix git wlroots wayland wayland-protocols libxkbcommon xorg-xwayland pipewire pipewire-pulse wireplumber bluez bluez-utils dunst foot ttf-dejavu swaybg grub efibootmgr
 
-input {
-    kb_layout = us
-}
+# Generate fstab
+echo "Generating fstab..."
+fstabgen -U /mnt >> /mnt/etc/fstab
 
-general {
-    gaps_in = 5
-    gaps_out = 10
-    border_size = 2
-    col.active_border = 0x66ee1111
-    col.inactive_border = 0x66333333
-}
+# Chroot into the new system for configuration
+echo "Configuring the system..."
+artix-chroot /mnt /bin/bash <<EOF
 
-decoration {
-    rounding = 5
-    drop_shadow = yes
-    shadow_range = 10
-    shadow_render_power = 3
-}
+# Set hostname
+echo "artix" > /etc/hostname
 
-animations {
-    enabled = yes
-    animation = windows, 1, 3, default
-    animation = fade, 1, 3, default
-}
+# Set locale
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
 
-# Keybindings
-bind = SUPER, Return, exec, kitty
-bind = SUPER, Q, killactive
-bind = SUPER, left, movefocus, l
-bind = SUPER, right, movefocus, r
-bind = SUPER, up, movefocus, u
-bind = SUPER, down, movefocus, d
-bind = SUPER, 1, workspace, 1
-bind = SUPER, 2, workspace, 2
-bind = SUPER, 3, workspace, 3
-bind = SUPER, 4, workspace, 4
-bind = SUPER, 5, workspace, 5
-bind = SUPER, 6, workspace, 6
-bind = SUPER, 7, workspace, 7
-bind = SUPER, 8, workspace, 8
-bind = SUPER, 9, workspace, 9
-bind = SUPER, 0, workspace, 10
-bind = SUPER SHIFT, 1, movetoworkspace, 1
-bind = SUPER SHIFT, 2, movetoworkspace, 2
-bind = SUPER SHIFT, 3, movetoworkspace, 3
-bind = SUPER SHIFT, 4, movetoworkspace, 4
-bind = SUPER SHIFT, 5, movetoworkspace, 5
-bind = SUPER SHIFT, 6, movetoworkspace, 6
-bind = SUPER SHIFT, 7, movetoworkspace, 7
-bind = SUPER SHIFT, 8, movetoworkspace, 8
-bind = SUPER SHIFT, 9, movetoworkspace, 9
-bind = SUPER SHIFT, 0, movetoworkspace, 10
+# Set timezone (adjust as needed)
+ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
+hwclock --systohc
 
-# Execute on startup
-exec-once = swaybg -c #002b36
-exec-once = waybar
-exec-once = blueman-applet
+# Set root password
+echo "root:root" | chpasswd
+
+# Enable Arch mirrors for additional packages
+pacman -S --noconfirm artix-archlinux-support
+echo -e "\n[extra]\nInclude = /etc/pacman.d/mirrorlist-arch\n\n[multilib]\nInclude = /etc/pacman.d/mirrorlist-arch" >> /etc/pacman.conf
+pacman -Syu --noconfirm
+
+# Install GRUB
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=artix
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# Create user
+useradd -m -G wheel,audio,video,bluetooth -s /bin/bash user
+echo "user:user" | chpasswd
+
+# Set up sudo
+echo "%wheel ALL=(ALL) ALL" > /etc/sudoers.d/wheel
+
+# Clone and build DWL
+su - user -c "mkdir -p /home/user/.local/src"
+su - user -c "git clone https://codeberg.org/oceanicc/dwl /home/user/.local/src/dwl"
+su - user -c "cd /home/user/.local/src/dwl && [ -f config.def.h ] && cp config.def.h config.h"
+su - user -c "cd /home/user/.local/src/dwl && make && sudo make install"
+
+# Clone and build minibar
+su - user -c "git clone https://codeberg.org/oceanicc/minibar /home/user/.local/src/minibar"
+su - user -c "cd /home/user/.local/src/minibar && make && sudo make install"
+
+# Configure foot terminal
+mkdir -p /home/user/.config/foot
+cat << 'EOT' > /home/user/.config/foot/foot.ini
+[main]
+font=monospace:size=12
+[colors]
+background=002b36
+foreground=839496
+regular0=073642
+regular1=dc322f
+regular2=859900
+regular3=b58900
+regular4=268bd2
+regular5=d33682
+regular6=2aa198
+regular7=eee8d5
+EOT
+
+# Configure Dunst notification daemon
+mkdir -p /home/user/.config/dunst
+cat << 'EOT' > /home/user/.config/dunst/dunstrc
+[global]
+    font = Monospace 10
+    geometry = "300x5-30+20"
+    transparency = 10
+    frame_color = "#002b36"
+    separator_color = frame
+[urgency_low]
+    background = "#002b36"
+    foreground = "#839496"
+[urgency_normal]
+    background = "#073642"
+    foreground = "#eee8d5"
+[urgency_critical]
+    background = "#dc322f"
+    foreground = "#fdf6e3"
+EOT
+
+# Create DWL startup script
+mkdir -p /home/user/.local/bin
+cat << 'EOT' > /home/user/.local/bin/startdwl.sh
+#!/bin/sh
+swaybg -c '#002b36' &
+pipewire &
+pipewire-pulse &
+wireplumber &
+dunst &
+dwl -b /usr/local/bin/minibar
+EOT
+chmod +x /home/user/.local/bin/startdwl.sh
+
+# Enable services with Dinit
+dinitctl enable bluetoothd
+
+# Set ownership
+chown -R user:user /home/user/.config /home/user/.local
+
+# Exit chroot
+exit
 EOF
 
-# Write Waybar configuration (with pulseaudio and tray modules)
-cat << EOF > /home/$USER/.config/waybar/config
-{
-    "layer": "top",
-    "position": "top",
-    "height": 24,
-    "modules-left": ["hyprland/workspaces"],
-    "modules-center": [],
-    "modules-right": ["pulseaudio", "clock", "tray"],
-    "hyprland/workspaces": {
-        "format": "{name}"
-    },
-    "clock": {
-        "format": "{:%H:%M}"
-    },
-    "pulseaudio": {
-        "format": "{volume}% {icon}",
-        "format-icons": {
-            "default": ["", "", ""]
-        }
-    },
-    "tray": {
-        "spacing": 10
-    }
-}
-EOF
+# Unmount partitions
+echo "Unmounting partitions..."
+umount -R /mnt
 
-# Set ownership of config files
-chown -R $USER:$USER /home/$USER/.config/hypr
-chown -R $USER:$USER /home/$USER/.config/waybar
-
-# Enable Pipewire user services
-runuser -u $USER -- systemctl --user enable pipewire pipewire-pulse wireplumber
-
-# Completion message
-echo "Setup complete. Audio and Bluetooth have been configured with Pipewire and Blueman."
-echo "Please reboot the system to start using Hyprland with full audio and Bluetooth support."
-echo "For more information on using Hyprland, visit https://wiki.hyprland.org"
+echo "Installation complete! Reboot to use your new Artix system."
